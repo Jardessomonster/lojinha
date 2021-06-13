19fec1a84d47ee55db4dfa972700212af6d222a8
igormaia01 <igormaia2001@gmail.com>
2021-06-13 15:19:24 -0500

    Cadastro de Produto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>  
+    <label>nome</label>
+    <input type="text">
+    <label>senha</label>
+    <input type="spasword">
+    </form>
+    <input type="submmit">
+</body>
+</html>
