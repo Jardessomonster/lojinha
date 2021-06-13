8ef03dd006d3e819078fe0c85f0608ff9bda2e5b
Jardesson <55899358+Jardessomonster@users.noreply.github.com>
2021-06-13 15:33:42 -0500

    Update Lojinha.sql

--- Lojinha.sql
+++ Lojinha.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE Lojinha;
-use Lojinha;
+USE Lojinha;
 
 CREATE TABLE usuario(
     id INTEGER PRIMARY KEY,
     nome VARCHAR  (120) NOT NULL,
     email VARCHAR (160) NOT NULL,
-    senha PASSWORD NOT NULL
+    senha TEXT NOT NULL
 );
 
 CREATE TABLE produto(
